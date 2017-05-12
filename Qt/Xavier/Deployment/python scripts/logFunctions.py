Karpova-Lab/cerebro

commit f6687a0a26e862a77975c3d7376da24da8c25b6b

Compatible with Cerebro firmware 59’s cerebrolog. Added new plot that shows histogram of light level feedback

Qt/Xavier/Deployment/python scripts/logFunctions.py
@@ -38,26 +38,33 @@ def parseBaseStation(logFile):
     bLog.drop(bLog.columns[[2,3,4,5,6]], axis=1, inplace=True)          #remove  parameter columns
     first = (bLog.loc[0,'bTime'])                                   #store time offset
     bLog['bTime'] = bLog['bTime']-first                         #subtract offset from all timestamps
-    bLog['bDiff'] = bLog['bTime']-bLog['bTime'].shift(1)        #add column of time between events
+    bLog['bDiff'] = bLog['bTime'].diff()                            #add column of time between events
     bLog['Clock'] = bLog['bTime'].apply(readableTime)               #add column with readable time
     bData['sessionLength'] = bLog.loc[len(bLog)-1,'Clock']
     return bLog,bData
 
 def parseCerebroLog(logFile):
     # get cerebro attributes from cerebro log
-    cLog = pd.read_csv(logFile,names=['cTime','Received','extra1','extra2'])
-    cData = {'firmware':cLog.loc[0,'Received']} #get firmware version
-    cData['paramNames'],cData['waveform'] = getWaveformParamters(cLog[-26:-20]) #get list of soon to be column headings and a dictionary with the waveforms parameters
-    cData['waveform']['rampVector'] = getRampVector(cLog[-20:])
+    cLog = pd.read_csv(logFile,names=['cTime','Received','Light','extra2'])
 
-    cLog = cLog[1:-20]    #remove firwmare row, waveform parameters, ramp down vector
-    #label events
+    # get firmware and hardware info form first 3 lines
+    cData = {'firmware':cLog.loc[0,'Received']}
+    cData['cerebroNum'] = cLog.loc[1,'Received']
+    cData['laserdiodeNum'] = cLog.loc[2,'Received']
+
+    cData['paramNames'],cData['waveform'] = getWaveformParamters(cLog[-25:-20]) # get list of soon to be column headings and create a dictionary with the waveforms parameters
+    cData['waveform']['rampVector'] = getRampVector(cLog[-20:]) # add rampvector definition to waveform dictionary
+
+    cLog = cLog[3:-20]    #remove the hardware and firmware info as well as the ramp vector.
+
+    #label events with the correct pulse parameters.
     paramChangeIndices = cLog[cLog['cTime'].str.contains("Start_Delay")].index.tolist() #get indices of parameter changes
     #get parameters from the parameter rows and then use them to label the events above them that they describe
     for i,paramChangeIndex in enumerate(paramChangeIndices):
         for j,paramName in enumerate(cData['paramNames']):
             b = paramChangeIndices[i-1]+len(cData['paramNames']) if i>0 else 0 #b is the index of where above the parameters to start labeling events.
             cLog.loc[b:paramChangeIndex-1,paramName] = cLog.loc[paramChangeIndex+j,'Received']
+
     cLog['cTime'] =to_numeric(cLog['cTime'],errors='coerce')    #convert time column from strings to numbers
     cLog = cLog[cLog['cTime'].notnull()]                        #remove parameter rows from data frame, leaving of list of events w/parameter labels
     cLog = reIndex(cLog)
@@ -65,9 +72,9 @@ def parseCerebroLog(logFile):
     # Add cDiff column
     first = cLog.loc[0,'cTime']                                 #get time offset
     cLog['cTime'] = cLog['cTime']-first                         #subtract offset from all timestamps
-    cLog['cDiff'] = cLog['cTime']-cLog['cTime'].shift(1)        #add column of time between events
+    cLog['cDiff'] = cLog['cTime'].diff()                        #add column of time between events
 
-    cLog = cLog.drop(['extra1','extra2'], 1) #remove extra columns
+    cLog = cLog.drop(['extra2'], 1) #remove extra columns
     return cLog,cData
 
 def getWaveformParamters(_wavformDF):
@@ -86,7 +93,7 @@ def getRampVector(_rampDF):
 def compare(bLog,cLog,cerColHeaders):
     #combine the logs
     baseCols = ['Clock','Sent','bTime','bDiff']
-    cerebroCols = ['cDiff','cTime','Received'] + cerColHeaders
+    cerebroCols = ['cDiff','cTime','Received'] + cerColHeaders + ['Light']
     combined = concat([bLog[baseCols],cLog[cerebroCols]],axis=1,) # concatenate the two dataframes
 
 # **Compare the data logs to reveal any events may have been sent by Base Station, but not executed by Cerebro**
@@ -205,8 +212,9 @@ def writeSummary(cerebroLogPath,combined,baseData,cerebroData,compareData):
 
     dataOutput = {
         'Session_Start': baseData['startTime'],
-        'Cerebro': int(baseData['cerebroNum']),
-        'Rig': round(baseData['rigNum'],2),
+        'Cerebro': [int(baseData['cerebroNum']) , int(cerebroData['cerebroNum'])],
+        'Laser_diode': int(cerebroData['laserdiodeNum']),
+        'Rig': float(round(baseData['rigNum'],2)),
         'Base_Station_Firmware': int(baseData['baseFirmware']),
         'Cerebro_Firmware': int(cerebroData['firmware']),
         'Base_Station_Filter': int(baseData['filterDuration']),
@@ -235,6 +243,7 @@ def writeSummary(cerebroLogPath,combined,baseData,cerebroData,compareData):
         json.dump(dataOutput,saveJSON,indent=2,sort_keys=True)
 
 def matplotlibGraph(cerebroLogPath,bothDF,compData,sumry):
+    ###---Alignment Graph---###
     plotTitle = "Time Between\nLogged Events"
     yAxis = "Time since last event (minutes)"
     xAxis = "Event #"
@@ -248,6 +257,7 @@ def matplotlibGraph(cerebroLogPath,bothDF,compData,sumry):
     new = missedStop.append(missedTrig).append(missedContinue)
     new.sort_values(by='missIndex',inplace=True)
     reIndex(new)
+
 
     #plot points
     plt.figure(figsize=(17,11))
@@ -273,5 +283,19 @@ def matplotlibGraph(cerebroLogPath,bothDF,compData,sumry):
     plt.title(plotTitle,fontsize = 30)
     plt.ylabel(yAxis,fontsize = 25)
     plt.xlabel(xAxis,fontsize = 25)
-    plt.savefig('{}graph.png'.format(cerebroLogPath[:-14]))
+    plt.savefig('{}alignment.png'.format(cerebroLogPath[:-14]))
+
+    ###---Light Level Graph---###
+    plotTitle = "Light Level Distribution"
+    yAxis = "Fequency"
+    xAxis = "Light Level"
+    #plot points
+    plt.figure(figsize=(17,11))
+    plt.hist(bothDF['Light'][bothDF['Light']>0],bins=list(range(0,1030,10)))
+    plt.title(plotTitle,fontsize = 30)
+    plt.ylabel(yAxis,fontsize = 25)
+    plt.xlabel(xAxis,fontsize = 25)
+    # lightPlot, = plt.plot(range(len(bothDF)),bothDF['Light'],c='r',label="Light Level")
+    # lightLegend = plt.legend(handles=[lightPlot], loc=1,fontsize=20)
+
     plt.show()

commit 1cf53012251c7462ff33b57d716a7e2efdf52873

Xavier and parsing updates Xavier: 	-Added option to show histogram at the end of session. 	-Updated documentation web address 	-Base station filter should not include fade time!!! Parsing scripts: 	-Made plots smaller 	-Made compatible with Xavier’s option to show histogram 	-Fixed out of index bug (line 136) that was introduced in [f6687a0] because the power level is no longer a heading and therefore all of the cerColHeader indices were off

Qt/Xavier/Deployment/python scripts/logFunctions.py
@@ -47,7 +47,7 @@ def parseCerebroLog(logFile):
     # get cerebro attributes from cerebro log
     cLog = pd.read_csv(logFile,names=['cTime','Received','Light','extra2'])
 
-    # get firmware and hardware info form first 3 lines
+    # get firmware and hardware info from first 3 lines
     cData = {'firmware':cLog.loc[0,'Received']}
     cData['cerebroNum'] = cLog.loc[1,'Received']
     cData['laserdiodeNum'] = cLog.loc[2,'Received']
@@ -95,6 +95,7 @@ def compare(bLog,cLog,cerColHeaders):
     baseCols = ['Clock','Sent','bTime','bDiff']
     cerebroCols = ['cDiff','cTime','Received'] + cerColHeaders + ['Light']
     combined = concat([bLog[baseCols],cLog[cerebroCols]],axis=1,) # concatenate the two dataframes
+    # combined.to_csv('out.csv', sep=',')
 
 # **Compare the data logs to reveal any events may have been sent by Base Station, but not executed by Cerebro**
 
@@ -133,8 +134,8 @@ def compare(bLog,cLog,cerColHeaders):
             missDiff.append(abs(percentDiff-correctDiff))
             combined[cerebroCols] = combined[cerebroCols][:j].append(combined[cerebroCols][j:].shift(1))
             if combined['Sent'][j] == "Trigger Sent":
-                singleDuration = int(combined[cerColHeaders[1]][j+1])+int(combined[cerColHeaders[4]][j+1])
-                trainDuration = int(combined[cerColHeaders[3]][j+1])
+                singleDuration = int(combined["On_Time"][j+1])+int(combined["Ramp_Duration"][j+1])
+                trainDuration = int(combined["Train_Duration"][j+1])
                 trueDuration = max(singleDuration,trainDuration) #duration that cerebro will be busy executing a waveform
                 if int(baseInterval)<trueDuration:
                     ignoreVec.append(j)
@@ -242,7 +243,7 @@ def writeSummary(cerebroLogPath,combined,baseData,cerebroData,compareData):
     with open(cerebroLogPath[:-14]+'summary.json','w') as saveJSON:
         json.dump(dataOutput,saveJSON,indent=2,sort_keys=True)
 
-def matplotlibGraph(cerebroLogPath,bothDF,compData,sumry):
+def showAlignmentPlot(cerebroLogPath,bothDF,compData,sumry):
     ###---Alignment Graph---###
     plotTitle = "Time Between\nLogged Events"
     yAxis = "Time since last event (minutes)"
@@ -258,9 +259,8 @@ def matplotlibGraph(cerebroLogPath,bothDF,compData,sumry):
     new.sort_values(by='missIndex',inplace=True)
     reIndex(new)
 
-
     #plot points
-    plt.figure(figsize=(17,11))
+    plt.figure(figsize=(11,8.5))
     bPlot, = plt.plot(range(len(bothDF)),bothDF['bDiff']/60000,'g',label="Base Station")
     cPlot, = plt.plot(range(len(bothDF)),bothDF['cDiff']/60000,'b',label="Cerebro",alpha = 0.5)
 
@@ -272,26 +272,28 @@ def matplotlibGraph(cerebroLogPath,bothDF,compData,sumry):
         plt.axvspan(miss-1, miss+1, color='red', alpha=0.5)
         missString+=("{} @ {}\n".format(missType, miss))
     first_legend = plt.legend(handles=[bPlot, cPlot], loc=1,fontsize=20)
-    plt.annotate(missString,xy=(0.005,.99),xycoords='axes fraction',size=12,verticalalignment = 'top')
+    plt.annotate(missString,xy=(0.005,.99),xycoords='axes fraction',size=10,verticalalignment = 'top')
 
     #Display session summary at the top
     totalSummary = '{}{}\n{}\n{}'.format(sumry['lengthSummary'],sumry['triggerSummary'],sumry['continueSummary'],sumry['abortSummary'])
     totalSummary = totalSummary.replace("\t\t", "\t")
     totalSummary = totalSummary.replace("\t", "      ")
-    plt.figtext(.125,.92,totalSummary,fontsize = 12,verticalalignment = 'bottom')
+    plt.figtext(.125,.92,totalSummary,fontsize = 8,verticalalignment = 'bottom')
 
     plt.title(plotTitle,fontsize = 30)
     plt.ylabel(yAxis,fontsize = 25)
     plt.xlabel(xAxis,fontsize = 25)
-    plt.savefig('{}alignment.png'.format(cerebroLogPath[:-14]))
+    # plt.savefig('{}alignment.png'.format(cerebroLogPath[:-14]))
+    plt.show()
 
+def showHistogramPlot(bothDF):
     ###---Light Level Graph---###
     plotTitle = "Light Level Distribution"
     yAxis = "Fequency"
     xAxis = "Light Level"
     #plot points
-    plt.figure(figsize=(17,11))
-    plt.hist(bothDF['Light'][bothDF['Light']>0],bins=list(range(0,1030,10)))
+    plt.figure(figsize=(11,8.5))
+    plt.hist(bothDF['Light'][bothDF['Light']>0])
     plt.title(plotTitle,fontsize = 30)
     plt.ylabel(yAxis,fontsize = 25)
     plt.xlabel(xAxis,fontsize = 25)

Qt/Xavier/Deployment/python scripts/parseLogs.py
@@ -1,15 +1,17 @@
 from logFunctions import*
 from sys import argv
-script, basestationLogPath, cerebroLogPath, showGraph = argv
-
-showGraph = int(showGraph)
+script, basestationLogPath, cerebroLogPath, showData = argv
 
 baseDF,baseData = parseBaseStation(basestationLogPath)
 cerebroDF,cerebroData = parseCerebroLog(cerebroLogPath)
 combinedDF,compareData = compare(baseDF,cerebroDF,cerebroData['paramNames'])
 summaryStrings = printSummary(combinedDF,baseData['sessionLength'],cerebroData,compareData)
 
-if showGraph:
-    matplotlibGraph(cerebroLogPath,combinedDF,compareData,summaryStrings)
-else:
+if showData == "alignment and histogram":
+    showAlignmentPlot(cerebroLogPath,combinedDF,compareData,summaryStrings)
+    showHistogramPlot(combinedDF)
+elif showData == "save only":
+    writeSummary(cerebroLogPath,combinedDF,baseData,cerebroData,compareData)
+elif showData == "save and histogram":
+    showHistogramPlot(combinedDF)
     writeSummary(cerebroLogPath,combinedDF,baseData,cerebroData,compareData)

Qt/Xavier/mainwindow.cpp
@@ -59,7 +59,7 @@ MainWindow::MainWindow(QWidget *parent)
 
     aboutDialog = new QMessageBox();
         aboutDialog->setWindowTitle("About");
-        QString aboutString = "\t1.30.0\nUpdated:\t5/5/2017";
+        QString aboutString = "\t1.30.1\nUpdated:\t5/12/2017";
         aboutDialog->setText("Version:"+aboutString);
         aboutDialog->setStandardButtons(QMessageBox::Close);
 
@@ -496,6 +496,7 @@ void MainWindow::applySettings()
     //
     settings.beginGroup("Features");
         pythonEnabled = settings.value("pythonEnabled").toBool();
+        showHistogram = settings.value("showHistogram").toBool();
         mcubeEnabled = settings.value("mcubeEnabled").toBool();
     settings.endGroup();
     toolMenu->setEnabled(pythonEnabled);
@@ -1004,7 +1005,14 @@ void MainWindow::saveFile()
                 // Run python script to summarize data from base station and cerebro logs
                 QProcess *process = new QProcess(this);
                 QStringList pythonArgs;
-                pythonArgs<<qApp->applicationDirPath()+"/python scripts/parseLogs.py"<<saveName1<<saveName2<<"0"; //pass the two log file locations into the python script
+                QString plotArg;
+                if (showHistogram){
+                    plotArg = "save and histogram";
+                }
+                else{
+                    plotArg = "save only";
+                }
+                pythonArgs<<qApp->applicationDirPath()+"/python scripts/parseLogs.py"<<saveName1<<saveName2<<plotArg; //pass the two log file locations into the python script
                 process->start("python",pythonArgs);
                 process->waitForFinished(-1);
                 QString errorString = process->readAllStandardError();
@@ -1050,7 +1058,7 @@ void MainWindow::errorMsg()
 
 void MainWindow::updateFilter(){
     if (singleShot->isChecked()){
-        baseFilter = startDelay_spn->value() + onTime_spn->value()+fade_spn->value() * fade_checkbox->isChecked();
+        baseFilter = startDelay_spn->value() + onTime_spn->value();
     }
     else{
         baseFilter = startDelay_spn->value()+ trainDuration_spn->value();
@@ -1142,7 +1150,7 @@ void MainWindow::fadeChecked(){
 
 void MainWindow::openDocs(){
 //    opens readthedocs.com documentation
-    QUrl site = QUrl::fromEncoded( "http://cerebro.readthedocs.io/en/latest/Software/Xavier.html#xavier-user-guide");
+    QUrl site = QUrl::fromEncoded( "https://karpova-lab.github.io/cerebro/Software/Xavier.html");
     QDesktopServices::openUrl(site);
 //    //opens local html file
 //    QProcess *process = new QProcess(this);
@@ -1167,7 +1175,7 @@ void MainWindow::getGraphs()
             //Run python script for creating graph
             QProcess *process = new QProcess(this);
             QStringList pythonArgs;
-            pythonArgs<<qApp->applicationDirPath()+"/python scripts/parseLogs.py"<<baseData<<cerData<<"1"; //pass the two log file locations into the python script
+            pythonArgs<<qApp->applicationDirPath()+"/python scripts/parseLogs.py"<<baseData<<cerData<<"alignment and histogram"; //pass the two log file locations into the python script
             process->start("python",pythonArgs);
             process->waitForFinished(-1);
             QString errorString = process->readAllStandardError();
@@ -1190,7 +1198,6 @@ void MainWindow::getGraphs()
         }
     }
 }
-
 
 void MainWindow::setDebug()
 {

Qt/Xavier/mainwindow.h
@@ -59,7 +59,7 @@ private:
     QSerialPort*            serial,*serial2;
     QTimer*                 timer;
     settingsDialog          *settingsDlog;
-    bool                    pythonEnabled,mcubeEnabled;
+    bool                    pythonEnabled,mcubeEnabled,showHistogram;
     bool                    errorThrown;
     int                     baseFilter;
     QString                 startTime;

Qt/Xavier/settingsDialog.cpp
@@ -117,6 +117,9 @@ settingsDialog::settingsDialog(QWidget *parent)
             pythonCheckbox = new QCheckBox("Enable Python Scripts");
             pythonCheckbox->setChecked(pythonEnabled);
         featuresLayout->addWidget(pythonCheckbox);
+            histogramCheckbox  = new QCheckBox("Show histogram of light levels at the end of the session");
+            histogramCheckbox->setChecked(showHistogram);
+        featuresLayout->addWidget(histogramCheckbox);
             mcubeCheckbox = new QCheckBox("Check this box if using Xavier with MCUBE (without photoresistor feedback)");
             mcubeCheckbox->setChecked(mcubeEnabled);
         featuresLayout->addWidget(mcubeCheckbox);
@@ -156,6 +159,7 @@ settingsDialog::settingsDialog(QWidget *parent)
     connect(changeLabel_btn,SIGNAL(clicked()),this,SLOT(addAlias()));
     connect(cancelChange_btn,SIGNAL(clicked()),editLabelDialog,SLOT(close()));
     connect(okButton,SIGNAL(clicked()),this,SLOT(close()));
+    connect(pythonCheckbox,SIGNAL(clicked(bool)),histogramCheckbox,SLOT(setEnabled(bool)));
 
 }
 
@@ -175,6 +179,7 @@ void settingsDialog::saveChanges(){
 
     settings.beginGroup("Features");
         settings.setValue("pythonEnabled",pythonCheckbox->isChecked());
+        settings.setValue("showHistogram",histogramCheckbox->isChecked());
         settings.setValue("mcubeEnabled",mcubeCheckbox->isChecked());
     settings.endGroup();
 
@@ -283,10 +288,12 @@ void settingsDialog::openSettings()
     //--Other Settings--//
     settings.beginGroup("Features");
         pythonEnabled = settings.value("pythonEnabled").toBool();
+        showHistogram = settings.value("showHistogram").toBool();
         mcubeEnabled = settings.value("mcubeEnabled").toBool();
     settings.endGroup();
     //set the checkboxes to match what we got from memory
     pythonCheckbox->setChecked(pythonEnabled);
+    histogramCheckbox->setChecked(showHistogram);
     mcubeCheckbox->setChecked(mcubeEnabled);
 
     this->exec();

Qt/Xavier/settingsDialog.h
@@ -49,9 +49,8 @@ public:
 
     QGroupBox*              featuresBox;
     QGridLayout*            featuresLayout;
-    QCheckBox*              pythonCheckbox,*mcubeCheckbox;
-    bool                    pythonEnabled;
-    bool                    mcubeEnabled;
+    QCheckBox*              pythonCheckbox,*mcubeCheckbox,*histogramCheckbox;
+    bool                    pythonEnabled,mcubeEnabled,showHistogram;
     QStringList             rigList,ratList,cerebroList;
 
     QPushButton*            okButton;