d5655192783ad68dbe78f358394044459a0ebf38
Andy Lustig <alustig3@gmail.com>
2017-09-07 22:02:43 -0400

    independently control left and right diodes with separate feedbacks!

--- Arduino/Cerebro_Sketches/cerebro/cerebro.ino
+++ Arduino/Cerebro_Sketches/cerebro/cerebro.ino
@@ -23,53 +23,30 @@ SOFTWARE.
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 const byte version = 62;
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-// #define MCUBE //uncomment to disable feedback during a trigger
-// #define OLDBOARD //uncomment if uploading code to Cerebro 4.7 or older
-//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 /*
-
         ______                   __
        / ____/___   _____ ___   / /_   _____ ____
       / /    / _ \ / ___// _ \ / __ \ / ___// __ \
      / /___ /  __// /   /  __// /_/ // /   / /_/ /
      \____/ \___//_/    \___//_.___//_/    \____/
 
-Documentation for this project can be found at http://cerebro.readthedocs.io/
+Documentation for this project can be found at https://karpova-lab.github.io/cerebro/
 */
-// #include <SoftwareSerial.h>
 #include <avr/pgmspace.h>
 #include <SPI.h>
 
-// SoftwareSerial mySerial(7,5); //rx,tx pin6 pin8 on attiny84
 
-//--------------OPERATING PARAMETERS--------------------
-const int memorySize =     8169;   /*The number of bytes we will save to on the EEPROM. Each event recording is 4 bytes of data
-(3 for the timestamp integer and 1 for the event description character). Parameter changes are 9 bytes, 1 for the event description character
-and 8 for the former parameters.8192 bytes will provide space for 2730 events to be recorded.*/
-#define LOG_START 216
-#define FADE_START 16
-#define HARDWARE_START 12
 #define LEFT 0
 #define RIGHT 1
 
-// //IR sensor input//
-// #define IR_dirReg         DDRA
-// #define IR_inputReg       PINA
-// #define IR_pin            1       //attiny84 pin12
-// //BTN  input//
-// #define BTN_dirReg        DDRA
-// #define BTN_outputReg     PORTA
-// #define BTN_inputReg      PINA
-// #define BTN_pin           0       //attiny84 pin13
 //DAC pins setup//
-const int oneSelect = 6;
-const int twoSelect = 5;
-const int photoONE = A0;
-const int photoTWO = A1;
+const int leftSelect = 6;
+const int rightSelect = 5;
+const int leftPhotoFeedback = A0;
+const int rightPhotoFeedback = A1;
 
 const byte indicatorLED = 8; //attiny pin 5, Arduino language pin8
 
-const unsigned int maxpulselength = 1000;//maximum pulse length (in microseconds) that we will listen for. A signal longer than this maxpulselength is not a pulse and indicates the message is over
 const byte NUMPULSES = 88;               //maximum length of the message we can receive
 const byte irResolution = 4;             //# of microseconds that we will delay before checking the state of the IR sensor again
 #define NUMPARAM 5
@@ -79,99 +56,127 @@ const char string_2[] PROGMEM = "Off_Time,";
 const char string_3[] PROGMEM = "Train_Duration,";
 const char string_4[] PROGMEM = "Ramp_Duration,";
 
-#define ON_DELAY  0
-#define ON_TIME   1
-#define OFF_TIME  2
-#define TRAIN_DUR 3
-#define RAMP_DUR  4
-
 // Then set up a table to refer to your strings.
 const char* const parameterLabels[] PROGMEM = {string_0, string_1, string_2, string_3, string_4};
 char label[20];            // make sure this is large enough for the largest string it must hold
 unsigned int waveform[NUMPARAM] = {};
-unsigned int onDelay = 2000;
-char marksReceived;
-unsigned int address = LOG_START;  /*start at the 217th byte address of eeprom. The first 216 bytes are reserved for persistant variables that are recalled from session to session (waveform parameters,hardware parameters,fade vector)*/
-bool trigMatch = false;
-bool stopMatch = false;
 int error;
 const float KP = 0.2;
 const byte interval = 3;
 bool isSettled = false;
-bool isMaxed = false;
-bool implantMode = false;
-bool diodeMode = false;
-bool receivingFadeVector = false;
-bool receivingHardwareVector = false;
-bool receivingPowerTest = false;
-bool powerTestMode = false;
+// bool isMaxed = false;
 unsigned int tempPower = 0;
 byte vectorIndex = 0;
 unsigned int powerLevel;
 unsigned int cerebroNum;
 unsigned int LD;
 
-const char saveMemoryFlag = -1;
-const char memoryDumpFlag = -2;
-const char resetAddressFlag = -3;
-
-#ifdef MCUBE
-int DAClevel = 725;
-#else
 int DAClevel = 0;
-#endif
+
+
+class LaserDiode{
+public:
+  uint8_t slaveSelectPin;
+  uint8_t analogPin;
+  bool isMaxed;
+  int16_t DAClevel;  
+
+  LaserDiode(uint8_t _slavePin, uint8_t _analogPin);
+  bool off();
+  void sendDAC(unsigned int value);
+  void feedback(int setPoint);
+  void fade();
+};
+
+LaserDiode::LaserDiode(uint8_t _slavePin, uint8_t _analogPin){
+  slaveSelectPin = _slavePin;
+  analogPin = _analogPin;
+  isMaxed = false;
+  pinMode(slaveSelectPin,OUTPUT);
+  digitalWrite(slaveSelectPin,HIGH);
+}
+
+bool LaserDiode::off(){
+  Serial.println("laser off");
+  Serial.print(analogRead(analogPin));
+  Serial.print(',');
+  Serial.println(DAClevel);
+  SPI.beginTransaction(SPISettings(50000000, MSBFIRST, SPI_MODE0));
+  digitalWrite(slaveSelectPin,LOW);
+  SPI.transfer(64);                        //Power down command (page 13, table 1 of LTC2630-12 datasheet)
+  SPI.transfer(0);
+  SPI.transfer(0);
+  digitalWrite(slaveSelectPin,HIGH);
+  if (DAClevel==4095){
+    isMaxed = true;
+  }
+  DAClevel = 0;
+  SPI.endTransaction();   
+  return false;
+}
+
+void LaserDiode::sendDAC(unsigned int value) {
+  SPI.beginTransaction(SPISettings(50000000, MSBFIRST, SPI_MODE0));
+  digitalWrite(slaveSelectPin,LOW);  
+  SPI.transfer(48);                        //Write to and Update (Power up) DAC Register command (page 13, table 1 of LTC2630-12 datasheet)
+  SPI.transfer(value>>4);                  //shift high byte
+  SPI.transfer(value<<4 & 255);            //shift low byte
+  digitalWrite(slaveSelectPin,HIGH);
+  SPI.endTransaction();
+}
+
+void LaserDiode::feedback(int setPoint){
+  int photocellVoltage = analogRead(analogPin);
+  error = setPoint-photocellVoltage;
+  DAClevel = DAClevel+int(error*KP);
+  if (DAClevel>4095) {
+    DAClevel = 4095;
+  }
+  else if (DAClevel<0){
+    DAClevel = 0;
+  }
+}
+
+void LaserDiode::fade(){
+  // unsigned long fadeClock;
+  // unsigned int param1;
+  // for (int k = FADE_START; k < FADE_START+200 ; k+=2) {  //fade values are stored in addresses 16-216 (100 values,2 bytes each)
+  //   fadeClock = millis();
+  //   param1 = eepromReadByte(k)<<8;
+  //   feedback(word(param1|eepromReadByte(k+1)));
+  //   sendDAC(DAClevel);
+  //   while((millis()-fadeClock)<(waveform[RAMP_DUR]/100)){
+  //     //wait
+  //   }
+  // }
+}
+
+LaserDiode left(6,A0);
+LaserDiode right(5,A1);
 
 //---------function prototypes---------//
-void characterizeDiode();
-void characterizeImplant();
-void triggerEvent(unsigned int desiredPower,bool whichLaser, bool useFeedback=true );
-void feedback(int setPoint,int phototransistor);
-void fade();
-byte listenForIR(int timeout=0);
-void updateWaveform(uint16_t (&marks)[NUMPULSES]);
-unsigned int convertBIN(uint16_t (&marks)[NUMPULSES],byte numMarks=4,byte start=0);
-void recordEvent(byte letter);
-void eepromWriteByte( unsigned int writeAddress, byte data ) ;
-byte eepromReadByte( unsigned int readAddress );
-void save2EEPROM();
-void readAddresses(int start, int finish);
-void printEEPROM();
-void sendDAC(unsigned int value, int slaveSelect);
-bool laserOFF(int slaveSelect , int phototransistor);
-void updateFadeVector(uint16_t (&marks)[NUMPULSES],byte offset);
-void updateHardware(uint16_t (&marks)[NUMPULSES]);
-void printFadeVector();
-void printParameters();
-int getLightLevel();
-void recordLightLevel(int lightLevel);
+void triggerEvent(unsigned int desiredPower, LaserDiode* thediode, bool useFeedback=true );
 
 void setup() {
-  ///////////Analog setup////////////////////////////
-  // ADMUX = B00000010;       //Vcc used as analog reference reference. PA2 is pin 11 on ATtiny84. ADMUX explained in SECTION 16.13.1 of datasheet
-  // ADCSRA |= (1<<ADPS2) | (1<<ADPS1);  //set division factor of 64. ADC frequncy = 8Mhz/64=125khz (ADC needs to be in 50-200khz range)
-  // ADCSRA |= (1<<ADEN);                //enable the ADC
-  ////////////Digital setup//////////////////////////
 
-  pinMode(oneSelect,OUTPUT);
-  digitalWrite(oneSelect,HIGH);
-  pinMode(twoSelect,OUTPUT);
-  digitalWrite(twoSelect,HIGH);
+
   SPI.begin();
-  pinMode(indicatorLED,OUTPUT);       //Output
-  digitalWrite(indicatorLED,LOW);
-
   Serial.begin(115200);  
   Serial.println("hello. is this thing on?");
-  laserOFF(oneSelect,photoONE);
-  laserOFF(twoSelect,photoTWO);
-  
+  left.off();
+  right.off();
+  // laserOFF(leftSelect,leftPhotoFeedback);
+  // laserOFF(leftSelect,rightPhotoFeedback);  
 }
 
 void loop() {
   Serial.println("new loop");
   delay(1000);
-  triggerEvent(20,LEFT,true);
-  delay(1000);
-  triggerEvent(40,RIGHT,true);
+  triggerEvent(80,&left,true);
+  delay(1000);  
+  triggerEvent(80,&right,true);
+  
+//   delay(1000);
+//   triggerEvent(40,RIGHT,true);
 }
 
--- Arduino/Cerebro_Sketches/cerebro/laser.ino
+++ Arduino/Cerebro_Sketches/cerebro/laser.ino
@@ -1,162 +1,42 @@
-void characterizeDiode(){
-  // triggerEvent(50); //warmup
-  // delay(60000);
-  // triggerEvent(50); //~ 1mW
-  // delay(60000);
-  // triggerEvent(70); //~1.5 mW
-  // delay(60000);
-  // triggerEvent(90); //~2 mW
-
-}
-//
-void characterizeImplant(){
-  // bool firstMax = true;
-  // unsigned int dlay = 4000;
-  // delay(dlay);
-  // for (int testLevel = 500; testLevel<751; testLevel+=50){
-  //   triggerEvent(testLevel);
-  //   delay(dlay);
-  // }
-  // for (int testLevel = 760; testLevel<901; testLevel+=10){
-  //   triggerEvent(testLevel);
-  //   delay(dlay);
-  // }
-  // for (int testLevel = 905; testLevel<1026; testLevel+=5){
-  //   Serial.print(testLevel);
-	// 	Serial.print(",");
-  //   if(!isMaxed){
-  //     triggerEvent(testLevel);
-  //     delay(dlay);
-  //   }
-  //   else if(firstMax){  //trigger once more after being maxed
-  //     triggerEvent(testLevel);
-  //     firstMax = false;
-  //   }
-  //   else{
-  //     //do nothing
-  //   }
-  // }
-  // isMaxed = false; //reset isMaxed
-}
-
-void triggerEvent(unsigned int desiredPower,bool whichLaser,bool useFeedback){
-  int select;
-  int photoSignal;
-  if (whichLaser==LEFT){
-    select = oneSelect;
-    photoSignal = photoONE;
-  }
-  else{
-    select = twoSelect;
-    photoSignal = photoTWO;
-  }
-  isSettled = false;
+void triggerEvent(unsigned int desiredPower,LaserDiode* thediode,bool useFeedback){
   unsigned long onClock,offClock,trainClock,delayClock,alt=0;
   bool laserEnabled = true; //set flag for entering waveform loop
   bool newPulse = true;      //
-  bool triggerRecorded = false;
   delayClock=millis();              //reset clocks
-  byte rcvd = 0;
   unsigned int onDelay,onTime,offTime,trainDur,rampDur;
-  if (diodeMode){
-    onDelay = 0;
-    onTime = 2000;
-    offTime = 6000;
-    trainDur = 57000;
-    rampDur = 0;
-  }
-  else if (implantMode){
-    onDelay = 0;
-    onTime = 1000;
-    offTime = 0;
-    trainDur = 0;
-    rampDur = 0;
-  }
-  else{
-    onDelay = 0;
-    onTime = 1000;
-    offTime = 0;
-    trainDur = 0;
-    rampDur = 0;
-    // onDelay = waveform[ON_DELAY];
-    // onTime = waveform[ON_TIME];
-    // offTime = waveform[OFF_TIME];
-    // trainDur = waveform[TRAIN_DUR];
-    // rampDur  = waveform[RAMP_DUR];
-  }
+  onDelay = 0;
+  onTime = 1000;
+  offTime = 0;
+  trainDur = 0;
+  rampDur = 0;
+
   if (onDelay>0){
-    while ((millis()-delayClock)<waveform[ON_DELAY]){
-      // if (!(IR_inputReg & (1<<IR_pin))){
-      //   while(! (IR_inputReg & (1<<IR_pin))){
-      //     //wait until stop pulse is finished
-      //   }
-      //   rcvd = listenForIR(5000);
-      //   if (stopMatch){
-      //     laserEnabled = laserOFF();
-      //     if (address < memorySize) {       //record abort event
-      //       recordEvent('A');
-      //     }
-      //     stopMatch = false;
-      //   }
-      //   rcvd = 0;
-      // }
+    while ((millis()-delayClock)<onDelay){
+      //wait. be ready to stop if interrupted.
     }
   }
   onClock=trainClock=millis();
   while(laserEnabled){   
     // //check if another command (abort or continuation) has been sent since the trigger was activated
-    // if (!(IR_inputReg & (1<<IR_pin))){
-    //   while(! (IR_inputReg & (1<<IR_pin))){
-    //     //wait until stop pulse is finished
-    //   }
-    //   rcvd = listenForIR(5000);
-    //   if (trigMatch){
-    //     onClock = millis();
-    //     trainClock = millis();
-    //     if (address < memorySize) {       //record continue event
-    //       recordEvent('C');
-    //     }
-    //     trigMatch = false;
-    //   }
-    //   else if (stopMatch){
-    //     recordLightLevel(getLightLevel());
-    //     if (rampDur>0){
-    //       fade();
-    //     }
-    //     laserEnabled = laserOFF();
-    //     if (address < memorySize) {       //record abort event
-    //       recordEvent('A');
-    //     }
-    //     stopMatch = false;
-    //   }
-    //   rcvd = 0;
-    // }
+    // reset clock on continuation. or abort waveform.
     //else if onClock hasn't expired, turn on/keep on the laser
-    // else if ((millis()-onClock)<onTime){
     if ((millis()-onClock)<onTime){
-        
       if(!useFeedback){
-        sendDAC(desiredPower,select);
+        thediode->sendDAC(desiredPower);
       }
       else{
-        sendDAC(DAClevel,select);              //Laser on
+        thediode->sendDAC(desiredPower);              //Laser on
         if(alt%interval==0){              //it takes time for the photocell to respond, so only implement feedback every fourth loop
-          feedback(desiredPower,photoSignal);         //increase or decrease DAClevel to reach desired lightPower
+          thediode->feedback(desiredPower);      //increase or decrease DAClevel to reach desired lightPower
         }
         alt++;
-      }
-      if (!triggerRecorded){           //event has not yet been recorded
-        if (address < memorySize) {    //record trigger event
-          recordEvent('T');
-        }
-        triggerRecorded = true;
       }
       offClock = millis();
     }
     //else if offClock hasn't expired, turn off/keep off light
     else if((millis()-offClock)<offTime){
       if (newPulse){                   //if the laser is on then turn it off, otherwise do nothing (i.e. leave turned off)
-        newPulse = laserOFF(select,photoSignal);         //laserOn = false
+        newPulse = thediode->off();         //laserOn = false
       }
     }
     //else if trainClock hasn't expired, restart the light pulse
@@ -166,76 +46,12 @@ void triggerEvent(unsigned int desiredPower,bool whichLaser,bool useFeedback){
     }
     //else the end of the waveform has been reached. turn off the light.
     else{
-      recordLightLevel(getLightLevel());
       if (useFeedback){
-        if (rampDur>0 && !implantMode && !diodeMode && !powerTestMode){
-          fade();
+        if (rampDur>0){
+          thediode->fade();
         }
       }
-      laserEnabled = laserOFF(select,photoSignal);
+      laserEnabled = thediode->off();
     }
   }
-}
-
-void feedback(int setPoint, int phototransistor ){
-  int photocellVoltage = analogRead(phototransistor);
-  error = setPoint-photocellVoltage;
-  DAClevel = DAClevel+int(error*KP);
-  if (DAClevel>4095) {
-    DAClevel = 4095;
-  }
-  else if (DAClevel<0){
-    DAClevel = 0;
-  }
-}
-
-int getLightLevel(){
-  ADCSRA |= (1<<ADSC);                    //start analog conversion
-  loop_until_bit_is_clear(ADCSRA,ADSC);   //wait until conversion is done
-  return ADC;
-}
-
-void fade(){
-  // unsigned long fadeClock;
-  // unsigned int param1;
-  // for (int k = FADE_START; k < FADE_START+200 ; k+=2) {  //fade values are stored in addresses 16-216 (100 values,2 bytes each)
-  //   fadeClock = millis();
-  //   param1 = eepromReadByte(k)<<8;
-  //   feedback(word(param1|eepromReadByte(k+1)));
-  //   sendDAC(DAClevel);
-  //   while((millis()-fadeClock)<(waveform[RAMP_DUR]/100)){
-  //     //wait
-  //   }
-  // }
-}
-
-void sendDAC(unsigned int value, int slaveSelect) {
-  SPI.beginTransaction(SPISettings(50000000, MSBFIRST, SPI_MODE0));
-  digitalWrite(slaveSelect,LOW);  
-  SPI.transfer(48);                        //Write to and Update (Power up) DAC Register command (page 13, table 1 of LTC2630-12 datasheet)
-  SPI.transfer(value>>4);                  //shift high byte
-  SPI.transfer(value<<4 & 255);            //shift low byte
-  digitalWrite(slaveSelect,HIGH);
-  SPI.endTransaction();
-}
-
-bool laserOFF(int slaveSelect , int phototransistor){
-  Serial.println("laser off");
-  Serial.print(analogRead(phototransistor));
-  Serial.print(',');
-  Serial.println(DAClevel);
-  SPI.beginTransaction(SPISettings(50000000, MSBFIRST, SPI_MODE0));
-  digitalWrite(slaveSelect,LOW);
-  SPI.transfer(64);                        //Power down command (page 13, table 1 of LTC2630-12 datasheet)
-  SPI.transfer(0);
-  SPI.transfer(0);
-  digitalWrite(slaveSelect,HIGH);
-  #ifndef MCUBE
-  if (DAClevel==4095){
-    isMaxed = true;
-  }
-  DAClevel = 0;
-  #endif
-  SPI.endTransaction();   
-  return false;
 }
