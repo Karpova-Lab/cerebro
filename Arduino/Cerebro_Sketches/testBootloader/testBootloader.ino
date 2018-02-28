402ef82ebc585d6584be9c1e6fbecd799fecf03d
Andy Lustig <alustig3@gmail.com>
2018-02-28 18:36:55 -0500

    bootloader test update

--- Arduino/Cerebro_Sketches/testBootloader/testBootloader.ino
+++ Arduino/Cerebro_Sketches/testBootloader/testBootloader.ino
@@ -1,39 +1,50 @@
 
-int indicatorLED = A0;
 #include <SparkFunBQ27441.h>      //https://github.com/sparkfun/SparkFun_BQ27441_Arduino_Library
-
-
+#include <LaserDiode.h>
+const uint8_t indicatorLED = A0; //32u4 pin 36
+LaserDiode right(&DDRB,&PORTB,0,A4);
+LaserDiode left(&DDRD,&PORTD,2,A2);
+uint32_t timePassed = 0;
+uint16_t startupTime = 10000;
 void setup() {                
   Serial.begin(115200);
-  delay(15000);
-   // Indicator LED
+  timePassed = millis();
+  // Indicator LED
   pinMode(indicatorLED,OUTPUT);
   digitalWrite(indicatorLED,HIGH);
+  while(millis()-timePassed<startupTime){
+    Serial.println(((millis()-timePassed)%startupTime)/1000);
+    blink(100);
+    blink(100);
+    delay(600);
+  }
     //*** Battery Monitor ***//
   Serial.println("connecting with battery  monitor...");
   if (!lipo.begin()){
 	// If communication fails, print an error message and loop forever.
     Serial.println("Error: Unable to communicate with BQ27441.");
     while(1){ //blink error
-      digitalWrite(indicatorLED,HIGH);
-      delay(1000);
-      digitalWrite(indicatorLED,LOW);
-      delay(1000);
+      blink(100);
     }
   }
   Serial.println("Connected to BQ27441!");
+  digitalWrite(indicatorLED,HIGH);
   lipo.setCapacity(400);
   while(lipo.soc()==0){
+    Serial.print(".");
     delay(1);
-    //wait;
   }  
   Serial.println("Complete!");
-   // Indicator LED
-  pinMode(indicatorLED,OUTPUT);
-  digitalWrite(indicatorLED,HIGH);
+  digitalWrite(indicatorLED,LOW);
 }
 
-// the loop routine runs over and over again forever:
-void loop() {              // wait for a second
+void loop() {
+}
+
+void blink(int blinkDelay){
+  digitalWrite(indicatorLED,HIGH);
+  delay(blinkDelay);
+  digitalWrite(indicatorLED,LOW);
+  delay(blinkDelay);
 }
 
